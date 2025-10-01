6039fcd50248ecf88584b627c82bae05a2ec72ab
xiyangyang <16604593187@163.com>
2025-10-02 00:58:55 +0800

    feat:Added class Buffer

---
+++ include/Buffer.h
@@ -0,0 +1,22 @@
+#include<vector>
+#include<string>
+#include<unistd.h>
+class Buffer{
+private:
+    std::vector<char>_buffer;
+    size_t _readIndex;
+    size_t _writeIndex;
+    static const size_t kInitialSize=1024;
+    void ensureWritableBytes(size_t len);
+    
+public:
+    Buffer(size_t initialSize=kInitialSize);
+    size_t readableBytes() const;//获取有效数据的长度
+    size_t writableBytes() const;//获取可写入数据的长度
+    ssize_t readFd(int fd,int* savedErrno);//从Fd读取数据到Buffer
+    void retrieve(size_t len);//向前推进数据指针
+    char* begin() { return _buffer.data(); }
+    const char* begin() const { return _buffer.data(); }
+    size_t prependableBytes() const { return _readIndex; }
+    void append(const char* data,size_t len);
+};
---
+++ src/Buffer.cpp
@@ -0,0 +1,64 @@
+#include "Buffer.h"
+#include<sys/uio.h>
+#include<unistd.h>
+#include<algorithm>
+#include<cstring>
+#include<cerrno>
+Buffer::Buffer(size_t initialSize):_buffer(initialSize),_readIndex(0),_writeIndex(0){}
+size_t Buffer::readableBytes() const{
+    return _writeIndex-_readIndex;
+}
+size_t Buffer::writableBytes() const{
+    return _buffer.size()-_writeIndex;
+}
+void Buffer::ensureWritableBytes(size_t len){
+    if(writableBytes()>=len)return;
+    if(writableBytes()+prependableBytes()>=len){
+        char* read_ptr = begin() + _readIndex;
+        char* write_ptr=begin()+_writeIndex;
+        char* dest_ptr=begin();
+        std::move(read_ptr,write_ptr,dest_ptr);
+        size_t readable=_writeIndex-_readIndex;
+        _readIndex=0;
+        _writeIndex=readable;
+    }
+    else{
+        size_t _capacity=_writeIndex+len;
+        _buffer.resize(_capacity);
+    }
+}
+void Buffer::append(const char* data,size_t len){
+    ensureWritableBytes(len);
+    char* target=begin()+_writeIndex;
+    std::memcpy(target,data,len);
+    _writeIndex+=len;
+}
+ssize_t Buffer::readFd(int fd,int* savedErrno){
+    const size_t kExtraBufSize=65536;
+    char extra_buf[kExtraBufSize];//定义栈上备用缓冲区
+    struct iovec vec[2];//I/O向量结构体
+
+    const size_t internal_writable=writableBytes();
+    vec[0].iov_base=begin()+_writeIndex;//vec[0]可写入空间
+    vec[0].iov_len=internal_writable;
+
+    vec[1].iov_base=extra_buf;//vec[1]备用缓冲区
+    vec[1].iov_len=kExtraBufSize;
+
+    const int iovcnt =2;
+    const ssize_t n=::readv(fd,vec,iovcnt);//执行readv系统调用
+
+    //以下为错误检查部分
+    if(n<0){
+        *savedErrno=errno;
+        return -1;
+    }else if(n==0)return 0;
+    else if(n<=internal_writable){
+        _writeIndex+=n;
+    }else{
+        _writeIndex=_buffer.size();
+        const size_t excess=n-internal_writable;
+        append(extra_buf,excess);
+    }
+    return n;
+}
